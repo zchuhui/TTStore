d97fb6be2b0940cb1a7ed9274540c2dbb7b5d5c6
zchuhui <943339564@qq.com>
2017-11-30 18:06:46 +0800

    调整为管理

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import GoodsList from './../views/GoodsList';
-import Cart from '@/views/Cart';
+import GoodsList from './../views/GoodsList'
+import Cart from '@/views/Cart'
+import Home from '@/views/home'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/goodslist',
       name: 'goodsList',
       component: GoodsList,
     },
---
+++ src/views/home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        {{msg}}
+    </div>
+    
+</template>
+
+<script>
+import Layout from '@/components/Layout'
+
+export default {
+  name: "home",
+  components:{
+      Layout
+  },
+  data() {
+    return {
+      msg: "This is home"
+    };
+  },
+  methods: {}
+};
+</script>
